469a02be5ccd63f857222608deca0411060eed30
Miko <gamess19394@gmail.com>
2025-07-27 20:56:52 +0500

    Work with html and images

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
   root: './src',
@@ -8,5 +11,23 @@ export default defineConfig({
   },
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions:{
+      input: {
+        main: './src/index.html',
+      }
+    }
   },
+  plugins: [
+    injectHTML(),
+    ViteMinifyPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
 });
