18be09aeec3e86e371dc9a70abcdef39f7a42c56
Miko <gamess19394@gmail.com>
2025-08-16 15:10:55 +0500

    Added format script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import injectHTML from 'vite-plugin-html-inject';
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 export default defineConfig({
   root: './src',
@@ -12,11 +12,11 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-    rollupOptions:{
+    rollupOptions: {
       input: {
         main: './src/index.html',
-      }
-    }
+      },
+    },
   },
   plugins: [
     injectHTML(),
